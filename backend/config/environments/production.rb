e23590c679884d22db6b5aa012adf32d7905c918
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2023-04-01 19:08:39 +0800

    cors all

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = "/cable"
   config.action_cable.url = ENV["CABLE_URL"]
-  config.action_cable.allowed_request_origins = [ ENV["APP_URL"] ]
+  config.action_cable.allowed_request_origins = ["*"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
